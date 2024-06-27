378b7e42cd08e921db38afd3a7629b28eb3f1256
EduardoRE <rodested@yahoo.com>
2024-06-27 20:18:53 +0200

    ... working to add the test_encrypt_file

--- tests/test_secman_execution.py
+++ tests/test_secman_execution.py
@@ -1,8 +1,10 @@
+import os
+import sys
 import unittest
 import logging
 import subprocess
-import os
-import sys
+import shutil
+from importlib import util, import_module
 
 
 # TODO: Test 01: Secrets file with 3 secrets to encrypt, overwriting original file: 3 secrets must be encrypted
@@ -19,14 +21,19 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 #   python -m unittest -b -v
 #   python -m pytest -v --log-cli-level=INFO
 
+
 class TestSecmanCLI(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        # Set the environment variable for the master key password
+        os.environ["MKEYPASSWD"] = "FQRDX23t2Gp0C_BlpgOLG6-uHLxxAN4P2bl4qrp4sBY="
+        os.chdir("src/secman")
+
     def test_help_option(self):
         # Test: Run the main file and get the help message
 
         # Set the environment variable for the master key password
-        os.environ["MKEYPASSWD"] = "FQRDX23t2Gp0C_BlpgOLG6-uHLxxAN4P2bl4qrp4sBY="
         # Change the current working directory to src/secman
-        os.chdir("src/secman")
         # Run the secman.py script with the -h option to get help
         # with the same Python interpreter that is used to run this test
         result = subprocess.run(
@@ -34,10 +41,44 @@ class TestSecmanCLI(unittest.TestCase):
         )
         # Check if the help message is in the output
         # logging.info(result.stdout)
-        logging.info(result.returncode)
         # logging.info(result.stderr)
         # logging.info(sys.path)
         self.assertIn("usage:", result.stdout)
+
+    def test_encrypt_file(self):
+        # Test: Encrypt the file at tests/input/project_secrets_test_01inp.py
+
+        # Set the environment variable for the master key password
+        # Change the current working directory to src/secman
+        # Run the secman.py script to encrypt the file
+        # Replace 'source_file_path' and 'destination_file_path' with the actual file paths
+        shutil.copyfile(
+            "../../tests/input/project_secrets_test_01inp_base.py",
+            "../../tests/input/project_secrets_test_01inp.py",
+        )
+        result = subprocess.run(
+            [
+                sys.executable,
+                "secman.py",
+                "-e",
+                "-f",
+                "../../tests/input/project_secrets_test_01inp.py",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        # Check if the encryption was successful
+        self.assertEqual(result.returncode, 0)
+        # TODO: Add additional assertions to verify the encryption result
+        # import the module and check the encrypted secrets
+        #import_module(../../tests/input/project_secrets_test_01inp.py)
+        #config = sys.modules[module_name]
+        spec = util.spec_from_file_location("mm", "../../tests/input/project_secrets_test_01inp.py")
+        config = util.module_from_spec(spec)
+        spec.loader.exec_module(config)
+        variable_names = [name for name, value in config.__dict__.items() if not name.startswith('_')]
+        logging.info(config.__name__)
+        logging.info(variable_names)
 
 
 if __name__ == "__main__":
