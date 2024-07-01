86f8ccfb8ba99c4327aaecd536922ca211040658
EduardoRE <rodested@yahoo.com>
2024-07-01 16:20:05 +0200

    Testing configuration finished! Now you can the tests from VScode or from the project folder as: python -m unitest OR python -m pytest

--- tests/test_secman_execution.py
+++ tests/test_secman_execution.py
@@ -69,16 +69,127 @@ class TestSecmanCLI(unittest.TestCase):
         )
         # Check if the encryption was successful
         self.assertEqual(result.returncode, 0)
-        # TODO: Add additional assertions to verify the encryption result
         # import the module and check the encrypted secrets
         #import_module(../../tests/input/project_secrets_test_01inp.py)
         #config = sys.modules[module_name]
-        spec = util.spec_from_file_location("mm", "../../tests/input/project_secrets_test_01inp.py")
-        config = util.module_from_spec(spec)
-        spec.loader.exec_module(config)
-        variable_names = [name for name, value in config.__dict__.items() if not name.startswith('_')]
-        logging.info(config.__name__)
-        logging.info(variable_names)
+        #
+        # spec1 = util.spec_from_file_location("ev1", "../../tests/input/project_secrets_test_01out.py")
+        # config1 = util.module_from_spec(spec1)
+        # spec1.loader.exec_module(config1)
+        # variable_names1 = [name for name, value in config1.__dict__.items() if not name.startswith('_')]
+        # logging.info(variable_names1)
+        # spec2 = util.spec_from_file_location("ev2", "../../tests/input/project_secrets_test_01inp_encrypted.py")
+        # config2 = util.module_from_spec(spec2)
+        # spec2.loader.exec_module(config2)
+        # variable_names2 = [name for name, value in config2.__dict__.items() if not name.startswith('_')]
+        # logging.info(variable_names2)
+        # self.assertEqual(variable_names1, variable_names2)
+        file_locations = [
+            ("ev1", "../../tests/input/project_secrets_test_01out.py"),
+            ("ev2", "../../tests/input/project_secrets_test_01inp_encrypted.py")
+        ]
+
+        variable_names = []
+        for name, file_location in file_locations:
+            spec = util.spec_from_file_location(name, file_location)
+            config = util.module_from_spec(spec)
+            spec.loader.exec_module(config)
+            variables = {name: value for name, value in config.__dict__.items() if not name.startswith('_')}
+            variable_names.append(variables)
+        #logging.info(variable_names)
+        self.assertEqual(len(variable_names), 2)
+        # self.assertEqual(variable_names[0], variable_names[1])
+        # self.assertEqual(variable_names[::2], variable_names[1::2])
+
+    def test_encrypt_decrypt_file(self):
+        # Test: Decrypt the file at tests/input/project_secrets_test_01inp_encrypted.py
+
+        # Set the environment variable for the master key password
+        # Change the current working directory to src/secman
+        # Run the secman.py script to encrypt the file
+        # Replace 'source_file_path' and 'destination_file_path' with the actual file paths
+        shutil.copyfile(
+            "../../tests/input/project_secrets_test_01inp_base.py",
+            "../../tests/input/project_secrets_test_01inp.py",
+        )
+        result = subprocess.run(
+            [
+                sys.executable,
+                "secman.py",
+                "-e",
+                "-f",
+                "../../tests/input/project_secrets_test_01inp.py",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        # Check if the encryption was successful
+        self.assertEqual(result.returncode, 0)
+        # import the module and check the encrypted secrets
+        #import_module(../../tests/input/project_secrets_test_01inp.py)
+        #config = sys.modules[module_name]
+        #
+        # spec1 = util.spec_from_file_location("ev1", "../../tests/input/project_secrets_test_01out.py")
+        # config1 = util.module_from_spec(spec1)
+        # spec1.loader.exec_module(config1)
+        # variable_names1 = [name for name, value in config1.__dict__.items() if not name.startswith('_')]
+        # logging.info(variable_names1)
+        # spec2 = util.spec_from_file_location("ev2", "../../tests/input/project_secrets_test_01inp_encrypted.py")
+        # config2 = util.module_from_spec(spec2)
+        # spec2.loader.exec_module(config2)
+        # variable_names2 = [name for name, value in config2.__dict__.items() if not name.startswith('_')]
+        # logging.info(variable_names2)
+        # self.assertEqual(variable_names1, variable_names2)
+        file_locations = [
+            ("ev2", "../../tests/input/project_secrets_test_01inp_encrypted.py")
+        ]
+
+        variable_names = []
+        for name, file_location in file_locations:
+            spec = util.spec_from_file_location(name, file_location)
+            config = util.module_from_spec(spec)
+            spec.loader.exec_module(config)
+            variables = {name: value for name, value in config.__dict__.items() if not name.startswith('_')}
+            variable_names.append(variables)
+        # logging.info(variable_names)
+        # Assert that keys in variables are exactly: AAA, BBB, CCC
+        expected_keys = set(["MASTER_KEY_ENV", "AAA", "BBB", "CCC", "AAA_ENCRYPTED", "BBB_ENCRYPTED", "CCC_ENCRYPTED"])
+        variables_dict = {name: value for name, value in config.__dict__.items() if not name.startswith('_')}
+        logging.info(variables_dict.keys())
+        keys = set(variables_dict.keys())
+        assert keys == expected_keys, f"Keys in variables are not exactly {expected_keys}"
+        # self.assertEqual(variable_names[0], variable_names[1])
+        # self.assertEqual(variable_names[::2], variable_names[1::2])
+        result = subprocess.run(
+            [
+                sys.executable,
+                "secman.py",
+                "-d",
+                "-f",
+                "../../tests/input/project_secrets_test_01inp_encrypted.py",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        # Check if the encryption was successful
+        self.assertEqual(result.returncode, 0)
+        file_locations = [
+            ("ev1", "../../tests/input/project_secrets_test_01inp_base.py"),
+            ("ev2", "../../tests/input/project_secrets_test_01inp.py")
+        ]
+
+        variable_names = []
+        for name, file_location in file_locations:
+            spec = util.spec_from_file_location(name, file_location)
+            config = util.module_from_spec(spec)
+            spec.loader.exec_module(config)
+            variables = {name: value for name, value in config.__dict__.items() if not name.startswith('_')}
+            variable_names.append(variables)
+        # logging.info(variable_names)
+        self.assertEqual(len(variable_names), 2)
+        self.assertEqual(variable_names[0], variable_names[1])
+        self.assertEqual(variable_names[::2], variable_names[1::2])
+
 
 
 if __name__ == "__main__":
