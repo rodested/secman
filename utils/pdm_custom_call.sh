9b5c83b0314c6c3aa223f9edc8891e6769734463
EduardoRE <rodested@yahoo.com>
2024-07-05 16:00:35 +0200

    Improved the pdm custom shell command to work in macos

--- utils/pdm_custom_call.sh
+++ utils/pdm_custom_call.sh
@@ -9,17 +9,23 @@ pdm() {
       echo "PDM is not installed. Please install PDM first."
       return 1
     fi
+    
     # Attempt to fetch the PDM virtual environment activation command.
-    local activation_command=$(command pdm venv activate 2> /dev/null)
+    local activation_command
+    activation_command=$(pdm venv activate 2> /dev/null)
+    
     if [[ -z "$activation_command" ]]; then
       echo "No PDM virtual environment found. Please ensure a virtual environment is created."
       return 1
     fi
-    eval "source $activation_command"
+
+    # Activate the virtual environment
+    eval "$activation_command"
+
     echo "PDM virtual environment activated."
     echo "Use command 'deactivate' to exit the virtual environment."
   else
-    command pdm $@
+    command pdm "$@"
   fi
 }
 
